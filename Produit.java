c17b42e90ec9033755f6ff875f22813a657910c7
Spoutniki <nikita.manch3nko@gmail.com>
2022-04-29 14:45:04 +0200

    ajout de l'objet produit

---
+++ Produit.java
@@ -0,0 +1,25 @@
+public class Produit {
+    private int reference;
+    private String nom;
+    private int prix;
+    
+    public int getReference() {
+        return reference;
+    }
+    public void setReference(int reference) {
+        this.reference = reference;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public int getPrix() {
+        return prix;
+    }
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+}
